57e6387187dc7d1203aa7c0f408d75f8f288af22
Robert-Clark-1990 <robert.elliott.clark@outlook.com>
2021-03-26 13:23:40 +0000

    customised Project on Admin

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -2,4 +2,16 @@ from django.contrib import admin
 from .models import Project
 
 
-admin.site.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'keywords',
+        'link',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+
+admin.site.register(Project, ProjectAdmin)
