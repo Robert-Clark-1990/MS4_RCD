ee8e5e9faa9904151f9be74154c4d39572ac0d33
Robert-Clark-1990 <robert.elliott.clark@outlook.com>
2021-03-26 14:56:18 +0000

    add portfolio and project detail pages

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Project
 
-# Create your views here.
+
+def portfolio(request):
+    """ A view to show the portfolio, including sorting and search queries """
+
+    portfolio = Project.objects.all()
+
+    context = {
+        'portfolio': portfolio,
+    }
+
+    return render(request, 'portfolio/portfolio.html', context)
+
+
+def project_detail(request, project_id):
+    """ A view to individual portfolio project """
+
+    project = get_object_or_404(Project, pk=project_id)
+
+    context = {
+        'project': project,
+    }
+
+    return render(request, 'portfolio/project_detail.html', context)
