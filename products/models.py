fbd92552afa3316411ea75c5454e2db63aa5edaa
Robert-Clark-1990 <robert.elliott.clark@outlook.com>
2021-03-31 10:58:42 +0000

    add quantity boxes to shopping bag and product page

--- products/models.py
+++ products/models.py
@@ -20,7 +20,7 @@ class Product(models.Model):
     description = models.TextField()
     t_and_c = models.TextField(null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    is_digital = models.BooleanField(null=True, blank=True)
+    is_digital = models.BooleanField(default=False, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
