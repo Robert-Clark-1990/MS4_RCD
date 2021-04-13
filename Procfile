e7b9ca6fa9ff8fdc9ca2f21b304460715ee1d6c7
Robert-Clark-1990 <robert.elliott.clark@outlook.com>
2021-04-13 11:35:21 +0000

    deployment to heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn robertclarkdesign.wsgi:application
--- robertclarkdesign/settings.py
+++ robertclarkdesign/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'c1axjs5@4z8*cl1p!mm$_ocg5q+fucjdtrgj^qyg7v9**oq)2@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['robertclarkdesign.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -126,12 +126,17 @@ WSGI_APPLICATION = 'robertclarkdesign.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
