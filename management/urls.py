142463288d27a23ffbfeb963d87a6650762c721f
Robert-Clark-1990 <robert.elliott.clark@outlook.com>
2021-04-13 09:46:57 +0000

    add admin order history views and urls

--- management/urls.py
+++ management/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.management, name='management'),
+    path('order_history/', views.order_history, name='order_history'),
 ]
--- management/views.py
+++ management/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+
+from checkout.models import Order
 
 
 @login_required
@@ -11,3 +13,16 @@ def management(request):
         return redirect(reverse('home'))
 
     return render(request, 'management/management.html')
+
+
+@login_required
+def order_history(request):
+    """ A view to return all customer order history """
+    orders = Order.objects.all().order_by('-date')
+
+    template = 'management/order_history.html'
+    context = {
+        'orders': orders,
+    }
+
+    return render(request, template, context)
