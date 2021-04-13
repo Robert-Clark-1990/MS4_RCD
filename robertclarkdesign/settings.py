57a61f64410b1f7a650ab165a94d31bd334e5d9a
Robert-Clark-1990 <robert.elliott.clark@outlook.com>
2021-04-13 11:41:13 +0000

    removed secret key and set debug

--- robertclarkdesign/settings.py
+++ robertclarkdesign/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'c1axjs5@4z8*cl1p!mm$_ocg5q+fucjdtrgj^qyg7v9**oq)2@'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['robertclarkdesign.herokuapp.com', 'localhost']
 
